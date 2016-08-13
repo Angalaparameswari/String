cba209351bccf72d560f31e7482236a09a63140d
Angalaparameswari <angalaparameswaridevi@gmail.com>
2016-08-13 20:49:28 +0530

    Create VowCons.java

---
+++ VowCons.java
@@ -0,0 +1,14 @@
+package String;
+
+public class VowCons {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="guvi";
+		String s2="";
+	  s2=s.replaceAll("[aeiouAEIOU]","");
+		System.out.println(s2);
+
+	}
+
+}
